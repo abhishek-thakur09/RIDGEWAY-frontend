29fb8faef30ad057e75c8833641dadc101aa16be
Abhishek Thakur <abburajput7876@gmail.com>
2026-04-21 22:48:59 +0530

    ..

--- src/Helper/Api.js
+++ src/Helper/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://my-app-backend.onrender.com",
+  baseURL: "https://ridgeway-backend.onrender.com",
   withCredentials: true,   
 });
 
